31bb9c51f1f4db300be2166de6860dbd366eb5a4
khoohz <hz2000.khz@gmail.com>
2023-03-28 01:41:41 +0800

    added .github workflows

---
+++ part3/coursecontent copy/src/App.js
@@ -0,0 +1,106 @@
+import { useState, useEffect } from 'react'
+import Note from './components/Note'
+import noteService from './services/notes'
+import Notification from './components/Notification'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+    </div>
+  )
+}
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState() 
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  
+  const hook = () => {
+    noteService
+      .getAll() //initiate fetching of data from server
+      .then(initialNotes => { //event handler for the operation
+        setNotes(initialNotes)
+      })
+  }
+  
+  useEffect(hook, []) //[] specify how often the effect is run
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+  
+    noteService //send the obj back to the server
+      .create(noteObject)
+      .then(returnedNote => {
+          setNotes(notes.concat(returnedNote))
+          setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    //duplicate an object
+    const changedNote = { ...note, important: !note.important }
+    
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000) //set state to null after 5secs
+        setNotes(notes.filter(n => n.id !== id))
+      })
+    
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+    return (
+      <div>
+        <h1>Notes</h1> 
+        <Notification message={errorMessage} />
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all' }
+          </button>
+        </div>
+        <ul>
+          {notesToShow.map(note => 
+            <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+        </ul>
+        <form onSubmit={addNote}>
+          <input value={newNote} onChange={handleNoteChange} />
+          <button type="submit">save</button>
+        </form>   
+        <Footer />
+      </div>
+    )
+}
+
+export default App
---
+++ part3/phonebook backend/index.js
@@ -0,0 +1,132 @@
+const express = require('express') //built-in Node's web server module
+const app = express()
+
+app.use(express.json())
+
+const morgan = require('morgan')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const cors = require('cors')
+app.use(cors())
+
+// morgan(function (tokens, request, response) {
+//     return [
+//       tokens.method(request, response),
+//       tokens.url(request, response),
+//       tokens.status(request, response),
+//       tokens.response(request, response, 'content-length'), '-',
+//       tokens['response-time'](request, response), 'ms'
+//     ].join(' ')
+//   })
+
+morgan.token('body', (request, response) => JSON.stringify(request.body))
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+
+const maxId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(id => id.id))
+        : 0
+    return maxId
+}
+
+app.get('/info', (request, response) => {
+    const currentTime = Date().toLocaleString()
+
+    response.send(
+        `<div>
+            <div>Phonebook has info for ${maxId()} people</div>
+            <div>${currentTime}</div>
+        </div>`
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+
+    const id = Number(request.params.id)
+    const person = persons.find(note => note.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+
+})
+
+
+const generateId = () => {
+    const min = Math.ceil(maxId() + 1)
+    const max = Math.floor(1000)
+    const newId = Math.floor(Math.random() * (max - min) + min)
+    return newId
+}
+
+app.post('/api/persons', (request, response) => {
+    
+    const body = request.body
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    if(!body.name){
+        response.status(404).json({ 
+            error: 'name missing' 
+        })}
+    else if(!body.number){
+        response.status(404).json({ 
+            error: 'number missing' 
+        })}
+    else if(persons.find(persons => persons.name === person.name && persons.name.length > 0)){
+        response.status(404).json({ 
+            error: 'name must be unique'
+        })}
+    else {
+        persons = persons.concat(person)
+    }
+
+    response.json(persons)
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+
+    response.status(204).end()
+
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
