92392ef7a65b1e3b54dc050a4d8101a96c3fe173
khoohz <hz2000.khz@gmail.com>
2023-04-01 01:19:35 +0800

    added build dir to phonebook backend

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -2,6 +2,7 @@ const express = require('express') //built-in Node's web server module
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 
 const morgan = require('morgan')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
